dbeb21b179ab9c2ba021a94771260ad1ed0a2c29
Dimitrios Desyllas <ddesillas@freemail.gr>
2016-08-09 21:24:44 +0300

    Make a setup script

--- scripts/install2.sh
+++ scripts/install2.sh
@@ -1,10 +1,56 @@
 #!/bin/bash
 
-PIWIK_PATH="."
+#Path where all the files will be executed
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+SCRIPT_PATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"/..
+
+# Printing functions
+black='\E[30;40m'
+red='\E[31;40m'
+green='\E[32;40m'
+yellow='\E[33;40m'
+blue='\E[34;40m'
+magenta='\E[35;40m'
+cyan='\E[36;40m'
+white='\E[37;40m'
+
+
+cecho ()                     # Color-echo.
+                             # Argument $1 = message
+                             # Argument $2 = color
+{
+local default_msg="No message passed."
+                             # Doesn't really need to be a local variable.
+
+message=${1:-$default_msg}   # Defaults to default message.
+color=${2:-$black}           # Defaults to black, if not specified.
+
+  echo -e "$color"
+  echo "$message"
+  tput sgr0                      # Reset to normal.
+
+  return
+}
+
+cecho "Setting Up Piwik" $magenta
+cecho "Configuring piwik database settings" $cyan
+
+read -p "Instert a password for piwik's database: " PIWIK_MYSQL_ROOT_PASSWORD
+
+cecho "Configuring Email settings for reports." $cyan
+
+
 
 #Create .data folder with piwik required stuff
-if [ ! -f  ${PIWIK_PATH} ]; then
- mkdir -p ${PIWIK_PATH}
+if [ ! -f  ${SCRIPT_PATH} ]; then
+ mkdir -p ${SCRIPT_PATH}
 fi
 
 read  -p "Instert smtp host: " MAIL_HOST
@@ -12,7 +58,7 @@ read -p "Insert smtp port: " MAIL_PORT
 read -p "Insert your email username: " MAIL_USER
 read -p "Insert your email password: " MAIL_PASS
 
-cat > ${PIWIK_PATH}/ssmtp.conf << EOF
+cat > ${SCRIPT_PATH}/ssmtp.conf << EOF
 UseTLS=Yes
 UseSTARTTLS=Yes
 root=${MAIL_USER}
@@ -22,4 +68,17 @@ AuthUser=${MAIL_USER}
 AuthPass=${MAIL_PASS}
 EOF
 
-echo "www-data:${MAIL_USER}:${MAIL_HOST}:${MAIL_PORT}" >> ${PIWIK_PATH}/revaliases
+echo "www-data:${MAIL_USER}:${MAIL_HOST}:${MAIL_PORT}" >> ${SCRIPT_PATH}/revaliases
+
+cecho "Setting up wordpress" $magenta
+
+cecho "Setting up wordpress' database settings" $magenta
+
+read -p "Insert a password for database ROOT user: " WORDPRESS_MYSQL_ROOT_PASSWORD
+read -p "Insert a username for a NEW database user that wordpresss will use for connection: " WORDPRESS_MYSQL_USER
+read -p "Insert a password for the user created above: " WORDPRESS_MYSQL_PASSWORD
+
+echo "env PIWIK_MYSQL_ROOT_PASSWORD=${PIWIK_MYSQL_ROOT_PASSWORD} WORDPRESS_MYSQL_ROOT_PASSWORD=${WORDPRESS_MYSQL_ROOT_PASSWORD} WORDPRESS_MYSQL_USER=${WORDPRESS_MYSQL_USER} WORDPRESS_MYSQL_PASSWORD=${WORDPRESS_MYSQL_PASSWORD} docker compose -up -d" > $SCRIPT_PATH/start.sh
+chmod u+x $SCRIPT_PATH/start.sh
+
+cecho "Startup script ggenerated" $green
